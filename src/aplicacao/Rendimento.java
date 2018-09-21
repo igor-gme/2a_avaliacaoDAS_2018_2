ac03b0eec9e3de2f7d1921af2c587ab9b635a7bd
Igor Gabriel <igorgabriel515@gmail.com>
2018-09-21 17:44:57 -0300

    Barra verde para o 3 teste de Imposto.

--- src/aplicacao/Rendimento.java
+++ src/aplicacao/Rendimento.java
@@ -1,17 +1,28 @@
 package aplicacao;
 
 public class Rendimento {
-	
+
 	private float valorInicial;
 	private int prazo;
 	private float taxaAnual;
-	
+	private float aliquota;
+
 	public Rendimento(float valorInicial, int prazo, float taxaAnual) {
 		this.valorInicial = valorInicial;
 		this.prazo = prazo;
 		this.taxaAnual = taxaAnual;
+
+		if (prazo <= 180) {
+			aliquota = 22.5f/100;
+		}else if (prazo > 180 && prazo <= 360) {
+			aliquota = 20.0f/100;
+		}else if (prazo > 360 && prazo <= 720) {
+			aliquota = 17.5f/100;
+		}else {
+			aliquota = 15.0f/100;
+		}
 	}
-	
+
 	public float calcularRendimentoBruto() {
 		float taxaJurosAnual = taxaAnual/100;
 		float rendimentoBruto = (taxaJurosAnual * valorInicial) * prazo/365;
@@ -19,8 +30,8 @@ public class Rendimento {
 	}
 
 	public double calcularImpostodeRenda() {
-		
-		return 3.14f;
+
+		return calcularRendimentoBruto() * aliquota;
 	}
-	
+
 }
--- src/test/TesteImpostoDeRenda.java
+++ src/test/TesteImpostoDeRenda.java
@@ -12,7 +12,21 @@ public class TesteImpostoDeRenda {
 	public void testImpostoRenda() {
 		Rendimento calcRend = new Rendimento(1000.00f, 60, 8.5f);
 		
-		assertEquals(3.14f, calcRend.calcularImpostodeRenda(), 0.01);
+		assertEquals(3.14f, calcRend.calcularImpostodeRenda(), 0.01);		
+	}
+	
+	@Test
+	public void testImpostoRenda2() {
+		Rendimento calcRend = new Rendimento(500.00f, 120, 8.0f);
+		
+		assertEquals(2.96f, calcRend.calcularImpostodeRenda(), 0.01);
+		
+	}
+	@Test
+	public void testImpostoRenda3() {
+		Rendimento calcRend = new Rendimento(3000.00f, 240, 9.0f);
+		
+		assertEquals(35.51f, calcRend.calcularImpostodeRenda(), 0.01);
 		
 	}
 
