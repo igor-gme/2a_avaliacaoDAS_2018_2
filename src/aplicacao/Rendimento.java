8db2dd3620bd2d72a5dfcda69c06086f47ce34df
Igor Gabriel <igorgabriel515@gmail.com>
2018-09-21 17:40:40 -0300

    Teste de rendimento parametrozado.

--- src/aplicacao/Rendimento.java
+++ src/aplicacao/Rendimento.java
@@ -2,35 +2,39 @@ package aplicacao;
 
 public class Rendimento {
 	
+	private float valorInicial;
+	private int prazo;
+	private float taxaAnual;
 	
-	public Rendimento() {
-
+	public Rendimento(float valorInicial, int prazo, float taxaAnual) {
+		this.valorInicial = valorInicial;
+		this.prazo = prazo;
+		this.taxaAnual = taxaAnual;
 	}
-
 	
-	public float calcularRendimentoBruto(float valorInicial, int prazo, float taxaAnual) {
+	public float calcularRendimentoBruto() {
 		float taxaJurosAnual = taxaAnual/100;
-		float rendimentoBruto = (taxaJurosAnual* valorInicial) * prazo/365;
+		float rendimentoBruto = (taxaJurosAnual * valorInicial) * prazo/365;
 		return rendimentoBruto;
 	}
 
-	public double calcularImposto(float valorInicial, int prazo, float taxaJuros) {
-		double imposto;
-		double aliquota;
-		if (prazo <= 180) {
-			aliquota = 22.5/100;
-			imposto = this.calcularRendimentoBruto(valorInicial, prazo, taxaJuros) * (aliquota);
-		}else if (prazo > 180 && prazo <= 360) {
-			aliquota = 20.0/100;
-			imposto = this.calcularRendimentoBruto(valorInicial, prazo, taxaJuros) * (aliquota);
-		}else if (prazo > 360 && prazo <= 720) {
-			aliquota = 17.5/100;
-			imposto = this.calcularRendimentoBruto(valorInicial, prazo, taxaJuros) * (aliquota);
-		}else {
-			aliquota = 15.0/100;
-			imposto = this.calcularRendimentoBruto(valorInicial, prazo, taxaJuros) * (aliquota);
-		}
-		return imposto;
-	}
+//	public double calcularImposto(float valorInicial, int prazo, float taxaJuros) {
+//		double imposto;
+//		double aliquota;
+//		if (prazo <= 180) {
+//			aliquota = 22.5/100;
+//			imposto = this.calcularRendimentoBruto(valorInicial, prazo, taxaJuros) * (aliquota);
+//		}else if (prazo > 180 && prazo <= 360) {
+//			aliquota = 20.0/100;
+//			imposto = this.calcularRendimentoBruto(valorInicial, prazo, taxaJuros) * (aliquota);
+//		}else if (prazo > 360 && prazo <= 720) {
+//			aliquota = 17.5/100;
+//			imposto = this.calcularRendimentoBruto(valorInicial, prazo, taxaJuros) * (aliquota);
+//		}else {
+//			aliquota = 15.0/100;
+//			imposto = this.calcularRendimentoBruto(valorInicial, prazo, taxaJuros) * (aliquota);
+//		}
+//		return imposto;
+//	}
 	
 }
--- src/test/TesteRendimentoBruto.java
+++ src/test/TesteRendimentoBruto.java
@@ -2,42 +2,51 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import aplicacao.Rendimento;
 
+@RunWith(Parameterized.class)
 public class TesteRendimentoBruto {
 
-	@Test
+	@Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] { {1000.00f, 60, 8.5f, 13.97f}, 
+											  {500.00f, 120, 8.0f, 13.15f},
+											  {3000.00f, 240, 9.0f, 177.53f},
+											  {2000f, 270, 8.5f, 125.75f},
+											  {100f, 390, 7.5f, 8.01f},
+											  {250f, 420, 8.0f, 23.01f},
+											  {400f, 550, 8.0f, 48.22f},
+											  {800f, 670, 8.0f, 117.48f},
+											  {2500f, 700, 9.0f, 431.51f},
+											  {4200f, 900, 9.5f, 983.84f},
+											  {100f, 1000, 7.5f, 20.55f}
+											  });
+	}
+	
+	@Parameter
+	public float valorInicial;
+ 	@Parameter(1)
+ 	public int prazo;
+ 	@Parameter(2)
+	public float taxaAnual;
+ 	@Parameter(3)
+	public float rendimentoBrutoEsperado;
+	
+ 	@Test
 	public void testRendimento() {
-		Rendimento calcRend = new Rendimento();
+		Rendimento calcRend = new Rendimento(valorInicial, prazo, taxaAnual);
 		
-		assertEquals(13.97f, calcRend.calcularRendimentoBruto(1000.00f, 60, 8.5f), 0.01);
-		
-	}
-	
-	@Test
-	public void testRendimento2() {
-		Rendimento calcRend = new Rendimento();
-		
-		assertEquals(177.53f, calcRend.calcularRendimentoBruto(3000.00f, 240, 9.0f), 0.01);
+		assertEquals(rendimentoBrutoEsperado, calcRend.calcularRendimentoBruto(), 0.01);
 		
 	}
-	
-	@Test
-	public void testRendimento3() {
-		Rendimento calcRend = new Rendimento();
-		
-		assertEquals(13.15f, calcRend.calcularRendimentoBruto(500.00f, 120, 8.0f), 0.01);
-		
-	}
-	
-//	@Test
-//	public void testRendimento2() {
-//		Rendimento calcRend2 = new Rendimento();
-//		
-//		assertEquals(3.14f, calcRend2.calcularImposto(1000.00f, 60, 8.5f), 0.01);
-//		
-//	}
 
 }
