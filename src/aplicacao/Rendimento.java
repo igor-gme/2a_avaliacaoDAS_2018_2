9c523455fa59aa8074f62c9ec3b018504ef62773
Igor Gabriel <igorgabriel515@gmail.com>
2018-09-21 14:04:34 -0300

    obter barra vermelha por novo teste para a mesta unidade previamente implementada.

--- src/aplicacao/Rendimento.java
+++ src/aplicacao/Rendimento.java
@@ -2,24 +2,17 @@ package aplicacao;
 
 public class Rendimento {
 	
-	private String tipoRendimento;
-	private int tempo;
-	private float valor;
+//	private String tipoRendimento;
+//	private int tempo;
+//	private float valor;
 	
-	public Rendimento(String tipoRendimento, float valor, int tempo) {
-		this.tipoRendimento = tipoRendimento;
-		this.valor = valor;
-		this.tempo = tempo;
+	public Rendimento() {
+
 	}
 
-	public static Rendimento obterRendimentoBruto(String tipoRendimento, float valor, int tempo) {
-		
-		return new Rendimento(tipoRendimento, valor, tempo);
-	}
 	
-	public float calcularRendimentoBruto() {
+	public float calcularRendimentoBruto(float valorInicial, int prazo, float taxaAnual, float aliquota) {
 		
 		return 13.97f;
 	}
-
 }
--- src/test/TesteRendimentoBruto.java
+++ src/test/TesteRendimentoBruto.java
@@ -10,9 +10,17 @@ public class TesteRendimentoBruto {
 
 	@Test
 	public void testRendimento() {
-		Rendimento rend = Rendimento.obterRendimentoBruto("Rendimento Bruto", 1000.00f, 60);
+		Rendimento calcRend = new Rendimento();
 		
-		assertEquals(13.97f, rend.calcularRendimentoBruto(), 0.0f);
+		assertEquals(13.97f, calcRend.calcularRendimentoBruto(1000.00f, 60, 0.085f, 0.225f), 0.0f);
+		
+	}
+	
+	@Test
+	public void testRendimento2() {
+		Rendimento calcRend2 = new Rendimento();
+		
+		assertEquals(13.15f, calcRend2.calcularRendimentoBruto(1000.00f, 60, 0.085f, 0.225f), 0.0f);
 		
 	}
 
